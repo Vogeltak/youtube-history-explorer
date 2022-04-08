699d3e5ccef4ed14beef1aa220373d0088a68fb8
Max Crone <max.crone@brightcomputing.com>
2022-04-08 22:32:07 +0200

    Start implementation of html extractor

--- src/youtube_history_explorer/utils.py
+++ src/youtube_history_explorer/utils.py
@@ -1,6 +1,26 @@
 import re
-import sys
 import datetime
+
+from .models import WatchEvent
+
+
+def extract_watch_events(watch_history):
+    events = []
+
+    div_pattern = 'class="content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1"'
+    matches = [m for m in re.finditer(div_pattern, watch_history)]
+
+    for m in matches:
+        m_len = watch_history[m.start():].find('</div>')
+        m_text = watch_history[m.start():m.start()+m_len]
+        try:
+            video_id = re.search(r'(?<=watch\?v=).{11}', m_text).group()
+            datetime_str = re.search(r'(... \d{1,2}, \d{4}, \d:\d\d:\d\d .. [A-Z]+)', m_text).group()
+        except:
+            print('Failed to find video_id or datetime in:')
+            print(m_text)
+
+    return []
 
 
 def parse_iso_duration(iso_duration):
